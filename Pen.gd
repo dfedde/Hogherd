9b0aa403061d43877b90098b3a9258f4419dd384
dyan <dyan@squidcity.net>
2024-12-09 22:04:13 -0600

    use signals for sheep getting penned

--- Pen.gd
+++ Pen.gd
@@ -7,10 +7,7 @@ signal all_penned
 # var b = "text"
 var penned_sheep = []
 
-	
-func _physics_process(_delta):
-	for body in get_overlapping_bodies():
-		if body.is_in_group("Penable"):
-			if penned_sheep.find(body) == -1:
-				penned_sheep.append(body)
-				body.in_pen()
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Penable"):
+		penned_sheep.append(body)
+		body.in_pen()
