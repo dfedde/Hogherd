90a776043cc71fc9460cf36dc391fd29789cef0a
dyan <dyan@squidcity.net>
2024-12-26 17:22:13 -0600

    Finish basic refactoing of  direction logic

--- Lib/Sheep/Sheep.gd
+++ Lib/Sheep/Sheep.gd
@@ -1,7 +1,5 @@
 extends CharacterBody2D
-# Refactor:
-# change flee target to the first item in the scary group.
-# change flee target to a bool that starts the run.
+
 # TODO: handle mutiple flee targets
 @export var speed: int = 200
 @export var max_dist: int = 150
@@ -27,8 +25,9 @@ func in_pen():
 
 func _physics_process(_delta):
 	if fleeing:
-		var flee_target = get_tree().get_first_node_in_group("Scary")
-		velocity = Vector2(-speed_with_falloff(flee_target.position),0).rotated(position.angle_to_point(flee_target.position))
+		for scary in get_tree().get_nodes_in_group("Scary"):
+			# TODO: this only handles fleeying from the last one may need context steering
+			velocity = Vector2(-speed_with_falloff(scary.position),0).rotated(position.angle_to_point(scary.position))
 	$AnimatedSprite2D.set_motion(velocity)
 	move_and_slide()
 
