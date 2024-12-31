753cf3e0acb2b87a9c29beea72bb5a2a626f258b
dyan <dyan@squidcity.net>
2024-12-31 10:03:54 -0600

    use score only for direction not velocity

--- Lib/Sheep/Sheep.gd
+++ Lib/Sheep/Sheep.gd
@@ -26,12 +26,14 @@ func in_pen():
 func _physics_process(_delta):
 	if fleeing:
 		_rays = []
-		velocity = Vector2.ZERO
+		var sum = Vector2.ZERO
 		for scary in get_tree().get_nodes_in_group("Scary"):
 			var dir := Direction.new(position.angle_to_point(scary.position))
 			dir.danger = inverse_lerp(max_dist, 0, min(position.distance_to(scary.position), max_dist))
 			_rays.append(dir)
-			velocity += Vector2(dir.score()*speed,0).rotated(dir.angle)
+		for ray in _rays:	
+			sum += Vector2(ray.score(),0).rotated(ray.angle)
+		velocity = sum.normalized() * speed
 	$AnimatedSprite2D.set_motion(velocity)
 	move_and_slide()
 	queue_redraw()
@@ -39,7 +41,7 @@ func _physics_process(_delta):
 func _draw() -> void:
 	if debug:
 		for ray in _rays:
-			draw_line(Vector2.ZERO, Vector2(abs(ray.score()* 100), 0).rotated(ray.angle), Color.RED if ray.score() < 0 else Color.GREEN, 2)
+			draw_line(Vector2.ZERO, Vector2(abs(ray.score() * 100), 0).rotated(ray.angle), Color.RED if ray.score() < 0 else Color.GREEN, 2)
 		
 class Direction:
 	var angle: float
